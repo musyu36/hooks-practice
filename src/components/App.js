f3582b7fdbe8b48a47766c2da9822543c0c09c24
mimura <musyu36@gmail.com>
2020-05-22 20:57:05 +0900

    save state with localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import EventForm from "./EventForm";
@@ -9,15 +9,28 @@ import AppContext from "../contexts/AppContext";
 // indexのインポートはindexの部分を省略可
 // import reducer from "../reducers/index.js";
 
+const APP_KEY = "appWithRedux";
+
 const App = () => {
-  const initialState = {
-    events: [],
-    operationLogs: [],
-  };
+  // localStorageから値を取り出し
+  const appState = localStorage.getItem(APP_KEY);
+  // localStorageに値があればparseで文字列からオブジェクトに変換してから格納
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
 
   // 状態遷移をさせたい時にdispatchを呼ぶ,引数には少なくともactionを渡す
   // useReducer(reducer, 管理するデータ(今回はイベント一覧))
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  // useEffectでstateの値を監視
+  useEffect(() => {
+    // local storageに保存できるように文字列化して保存
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     // contextによる値の受け渡し
